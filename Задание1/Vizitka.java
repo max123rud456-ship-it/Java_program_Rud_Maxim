4be377c2c7004432398e0092f3c08f1552868308
Максим Рудь <m.rud@erp.local>
2026-09-02 14:12:14 +0500

    Задания

---
+++ Задание1/Vizitka.java
@@ -0,0 +1,11 @@
+public class Vizitka {
+    public static void main(String[] args) {
+        String name = "Maxim";
+        int age = 20;
+        String city = "Karaganda";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+}
---
+++ Задание1/peremenii.java
@@ -0,0 +1,9 @@
+public class peremenii {
+    public static void main(String[] args) {
+    int number = 67;
+    double price = 67.67;
+    boolean isStudent = true;
+    String subject = "javaaa";
+    char grade = 'A';
+    }
+}
